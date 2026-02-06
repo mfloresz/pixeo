865e28adc77d26f509ffc89ba8ec532a9cf13b31
zavalor <codeberg@galaxymoon.anonaddy.com>
2026-02-05 22:08:55 -0600

    Updates

--- assets/jszip.min-DaOMSwr3.js
+++ assets/jszip.min-DaOMSwr3.js
@@ -1,4 +1,4 @@
-import{c as vt,a as yt,g as Bt}from"./index-M6I4noe1.js";function Rt(ct,wt){for(var _=0;_<wt.length;_++){const N=wt[_];if(typeof N!="string"&&!Array.isArray(N)){for(const w in N)if(w!=="default"&&!(w in ct)){const u=Object.getOwnPropertyDescriptor(N,w);u&&Object.defineProperty(ct,w,u.get?u:{enumerable:!0,get:()=>N[w]})}}}return Object.freeze(Object.defineProperty(ct,Symbol.toStringTag,{value:"Module"}))}var xt={exports:{}};/*!
+import{c as vt,a as yt,g as Bt}from"./index-Cuo5oxMO.js";function Rt(ct,wt){for(var _=0;_<wt.length;_++){const N=wt[_];if(typeof N!="string"&&!Array.isArray(N)){for(const w in N)if(w!=="default"&&!(w in ct)){const u=Object.getOwnPropertyDescriptor(N,w);u&&Object.defineProperty(ct,w,u.get?u:{enumerable:!0,get:()=>N[w]})}}}return Object.freeze(Object.defineProperty(ct,Symbol.toStringTag,{value:"Module"}))}var xt={exports:{}};/*!
 
 JSZip v3.10.1 - A JavaScript class for generating and reading zip files
 <http://stuartk.com/jszip>
